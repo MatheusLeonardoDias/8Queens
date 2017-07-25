6a4bdcc1e6a309f4aab81b6ef45be858a90812ea
Luiz Guilherme Fonseca Rosa <luizguilhermefr@gmail.com>
2017-07-25 10:45:44 -0300

    print changes

--- src/src/pkg8queens/Board.java
+++ src/src/pkg8queens/Board.java
@@ -20,14 +20,20 @@ public class Board {
     }
 
     public void print() {
-        System.out.println("\n************************");
+        for (int i = 0; i < table.length; i++) {
+            System.out.print("***");
+        }
+        System.out.println("");
         for (int i = 0; i < table.length; ++i) {
             for (int j = 0; j < table.length; ++j) {
                 System.out.print("|" + (table[i][j] ? "X" : "0") + "|");
             }
             System.out.println();
         }
-        System.out.println("\n************************\n");
+        for (int i = 0; i < table.length; i++) {
+            System.out.print("***");
+        }
+        System.out.println("");
     }
 
     public boolean isValidSetup() {
